312ca75db66b4580516d11858c72a1f7234db944
Juan Landy <jlandyr@gmail.com>
2023-04-04 16:43:34 +0200

    ViewController now includes a CollectionView to slide left/right articles

--- Headlines/ViewController.swift
+++ Headlines/ViewController.swift
@@ -7,37 +7,80 @@
 //
 
 import UIKit
-import SDWebImage
 
 class ViewController: UIViewController {
-    @IBOutlet var imageView: UIImageView!
-    @IBOutlet var headlineLabel: UILabel!
-    @IBOutlet var bodyLabel: UILabel!
+    @IBOutlet weak var collectionView: UICollectionView!
+    private var articles : [Article] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        registerCollectionView()
         reload()
         Article.fetchArticles { _, _ in
             self.reload()
         }
     }
     
+    private func registerCollectionView(){
+        collectionView.register(UINib(nibName: String(describing: NewsCollectionViewCell.self), bundle: nil), forCellWithReuseIdentifier: NewsCollectionViewCell.reuseIdentifier)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+    }
+    
     func reload() {
-        guard let article = Article.all.first else { return }
-        headlineLabel.text = article.headline
-        bodyLabel.text = article.body
-        imageView.sd_setImage(with: article.imageURL)
+        articles.removeAll()
+        articles = Article.all
+        collectionView.reloadData()
     }
 
-    @IBAction func favouritesButtonPressed() {
+    private func showFavourites() {
         let vc = FavouritesViewController()
         let nc = UINavigationController(rootViewController: vc)
         present(nc, animated: true, completion: nil)
     }
     
-    @IBAction func starButtonPressed() {
-        guard let article = Article.all.first else {return}
+    private func saveFavorite(_ article:Article) {
         Article.save(article)
+    }
+}
+
+//MARK: - UICollectionViewDataSource
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return articles.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: NewsCollectionViewCell.reuseIdentifier, for: indexPath) as? NewsCollectionViewCell else {return UICollectionViewCell()}
+        cell.article = articles[indexPath.row]
+        cell.didSelectSetFavorites = { [weak self] in
+            guard let self = self else {return}
+            self.saveFavorite(self.articles[indexPath.row])
+        }
+        cell.didSelectOpenFavorites = { [weak self] in
+            guard let self = self else {return}
+            self.showFavourites()
+        }
+        return cell
+    }
+}
+
+//MARK: - UICollectionViewDelegate
+extension ViewController : UICollectionViewDelegate {
+    
+}
+
+//MARK: - UICollectionViewDelegateFlowLayout
+extension ViewController: UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: view.frame.height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets.init(top: 0, left: 0, bottom: 0, right: 0)
     }
 }
