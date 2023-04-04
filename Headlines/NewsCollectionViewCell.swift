bb3d2a9852265df79654dfa74aeddb7d5bc3f5be
Juan Landy <jlandyr@gmail.com>
2023-04-04 18:35:50 +0200

    animation included when adding/removing from favorites

--- Headlines/NewsCollectionViewCell.swift
+++ Headlines/NewsCollectionViewCell.swift
@@ -23,7 +23,7 @@ class NewsCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var buttonStar: UIButton!{
         didSet{
-            buttonStar.titleLabel?.text = ""
+            buttonStar.setTitle("", for: .normal)
         }
     }
     @IBOutlet weak var buttonFavorite: UIButton!
@@ -37,6 +37,8 @@ class NewsCollectionViewCell: UICollectionViewCell {
     }
     
     @IBAction func buttonStar(_ sender: UIButton) {
+        setStar()
+        animate()
         didSelectSetFavorites?()
     }
     
@@ -46,6 +48,32 @@ class NewsCollectionViewCell: UICollectionViewCell {
             headlineLabel.text = _article.headline
             bodyText.text = _article.body
             imageView.sd_setImage(with: _article.imageURL)
+            setStar()
         }
     }
+    
+    private func setStar(){
+        guard let _article = article else { return }
+        DispatchQueue.main.async { [weak self] in
+            guard let self = self else {return}
+            if _article.isFavorite() {
+                self.buttonStar.setImage(UIImage(imageLiteralResourceName: "favourite-on"), for: .normal)
+            }else {
+                self.buttonStar.setImage(UIImage(imageLiteralResourceName: "favourite-off"), for: .normal)
+            }
+        }
+    }
+    
+    private func animate(){
+        DispatchQueue.main.async {
+            UIView.animate(withDuration: 1.0, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 6.0, options: [], animations: {
+                self.buttonStar.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+            }, completion: {_ in
+                UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 0.1, initialSpringVelocity: 6.0, options: [], animations: {
+                    self.buttonStar.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+                }, completion: nil)
+            })
+        }
+    }
+    
 }
